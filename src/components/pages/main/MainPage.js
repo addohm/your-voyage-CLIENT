fc390d187e282143258723c47bd4a8c2a0a74a0a
enotowitch <en.enotowitch@gmail.com>
2023-12-27 10:56:16 +0200

    F: MainPage calm animation 1 ?

--- src/components/pages/main/MainPage.js
+++ src/components/pages/main/MainPage.js
@@ -18,52 +18,94 @@ import AppBg from "../../appBg/AppBg";
 export default function MainPage() {
     return (
         <>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <Banner />
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <ExperienceAndEducation />
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <div className="h50vh fcc">
                     <Quote title="Steve Jobs" text="“Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.“" />
                 </div>
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <Banner2 />
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <Tools />
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <FullWidthImg img={fullWidthImg1} />
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <Quote title="Confucius" text="“Choose a job you love, and you will never have to work a day in your life.“" />
             </Animation>
-            <Animation type="scale091">
+            <Animation
+                // type="scale091"
+                type="opacity"
+            >
                 <Reviews />
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <Megaphone />
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <ApplyFor />
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <JustImagine />
             </Animation>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <CoachCards />
             </Animation>
 
             {/* ! `margin top` for Quote */}
             <div className="h300"></div>
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <Quote title="Albert Schweitzer" text="“Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.“" />
             </Animation>
 
-            <Animation type="scale151">
+            <Animation
+                // type="scale151"
+                type="opacity"
+            >
                 <FullWidthImg img={fullWidthImg2} />
             </Animation>
             {/* fixed position */}
