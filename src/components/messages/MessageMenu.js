acf0997a6111d0da830bc829d725920b46a36650
enotowitch <en.enotowitch@gmail.com>
2024-01-03 09:40:45 +0200

    M: MessageMenu

--- src/components/messages/MessageMenu.js
+++ src/components/messages/MessageMenu.js
@@ -1,25 +1,28 @@
 import { useState } from "react";
 import Menu from "../LoginGoogle/Menu";
 import MessageEdit from "./MessageEdit";
-import { MoreVert, Close, ReplayOutlined } from '@mui/icons-material';
+import { ManageHistory, Close, ReplayOutlined } from '@mui/icons-material';
 import MessageDelete from "./MessageDelete";
 import timestampToUnix from "../../utils/timestampToUnix";
 
 export default function MessageMenu({ isMyMsg, isContentEditableSet, isContentEditable, _id, email, room, isDeleted, createdAt }) {
 
     const [isHovered, isHoveredSet] = useState(null)
-    const isMsgOlderThan5Mins = timestampToUnix(createdAt) < timestampToUnix(Date.now() - 5 * 60 * 1000)
+    const minutes = 5
+    const isMsgOlderThanNMins = timestampToUnix(createdAt) < timestampToUnix(Date.now() - minutes * 60 * 1000)
 
     return (
-        (!isMsgOlderThan5Mins && isMyMsg) &&
+        (!isMsgOlderThanNMins && isMyMsg) &&
         <div
             onMouseEnter={() => isHoveredSet(true)}
             onMouseLeave={() => isHoveredSet(false)}
         >
-            <MoreVert className="poa r-10 t-4" />
-            <Menu isVisible={isMyMsg && isHovered} className="poa r10 t-4 bg_white px15 brL" >
+            <div title={minutes + " mins"}>
+                <ManageHistory className="poa r-6 t-4" />
+            </div>
+            <Menu isVisible={isMyMsg && isHovered} className="poa r20 t-7 bg_white px5 brL" >
                 <MessageEdit isVisible={isMyMsg && !isDeleted} isContentEditableSet={isContentEditableSet} isContentEditable={isContentEditable} _id={_id} email={email} room={room} />
-                <MessageDelete Icon={() => <Close />} isVisible={isMyMsg && !isDeleted} _id={_id} email={email} room={room} />
+                <MessageDelete Icon={() => <Close style={{ marginTop: 3 }} />} isVisible={isMyMsg && !isDeleted} _id={_id} email={email} room={room} />
                 <MessageDelete Icon={() => <ReplayOutlined />} isRestoring={true} isVisible={isMyMsg && isDeleted} _id={_id} email={email} room={room} />
             </Menu>
         </div>
