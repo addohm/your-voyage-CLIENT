dfcd0c249ab64420907cf2939eb19543b68f3978
enotowitch <en.enotowitch@gmail.com>
2024-01-05 12:19:49 +0200

    M: Markdown: +zoomImg

--- src/components/textEditor/Markdown.js
+++ src/components/textEditor/Markdown.js
@@ -1,10 +1,26 @@
-import React from "react"
+import React, { useContext } from "react"
 import Markdown from "marked-react"
 import "./index.scss"
+import { Context } from "../../Context"
 
 export default function Markdown_({ children }) {
+
+	const { dialogSet } = useContext(Context)
+
+	function zoomImg(e) {
+		if (!e.target.src) return // not img
+		dialogSet({
+			show: true,
+			children:
+				<img
+					src={e.target.src}
+					style={{ width: "100vw", height: "100%" }}
+				/>
+		})
+	}
+
 	return (
-		<pre className="innerImgW100p">
+		<pre className="innerImgW100p" onClick={zoomImg}>
 			<Markdown>{children}</Markdown>
 		</pre>
 	)
