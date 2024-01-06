a61e3441f267a338ceaf8d53f2707c17bb7c4218
enotowitch <en.enotowitch@gmail.com>
2024-01-06 10:10:38 +0200

    !!!: socket.io;

--- src/components/messages/useSocket.js
+++ src/components/messages/useSocket.js
@@ -10,7 +10,7 @@ export default function useSocket(room, dbMessagesSet) {
 
     // ! socket send
     const [message, messageSet] = useState("") // input value
-    const socket = io.connect("https://your-voyage-life.onrender.com")
+    const socket = io.connect(process.env.SERVER_URL)
 
     function sendMessage() {
         if (!message) return
