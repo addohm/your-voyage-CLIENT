d48910607ce212caca1be29ca9fd63f88cb986b0
enotowitch <en.enotowitch@gmail.com>
2024-01-07 12:36:07 +0200

    M: Room: +last msg img

--- src/components/messages/Room.js
+++ src/components/messages/Room.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import timestampToTime from "../../utils/timestampToTime";
 import Markdown from "../textEditor/Markdown";
 
-export default function Room({ img, name, room, msg, createdAt, notReadNum }) {
+export default function Room({ img, name, room, msg, createdAt, notReadNum, msgImg }) {
     return (
         <Link to={`/message/${room}`} className="f fwn g15 maw500 mb bg_white p brL">
             <div className="por">
@@ -11,7 +11,10 @@ export default function Room({ img, name, room, msg, createdAt, notReadNum }) {
             </div>
             <div className="fc black">
                 <div className="fw600 mt3">{name}</div>
-                <div className="textLines2"><Markdown>{msg}</Markdown></div>
+                <div className="f fwn g10 mt10">
+                    {msgImg && <img className="w40 h40 brL" src={msgImg} />}
+                    <div className="textLines2 mt10"><Markdown>{msg}</Markdown></div>
+                </div>
             </div>
             {createdAt && <div className="mla">{timestampToTime(createdAt)}</div>}
         </Link>
