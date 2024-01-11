06046dd9071f48eae6e575be32d84c38f7b8e727
enotowitch <en.enotowitch@gmail.com>
2024-01-11 13:13:27 +0200

    M: MessageFiles: +user download (eg zip) file; !!F: useUploadedPastedDroppedFilesToMsgFiles: remove dots from file name

--- src/components/messages/MessageFiles.js
+++ src/components/messages/MessageFiles.js
@@ -1,5 +1,14 @@
 export default function MessageFiles({ file }) {
+
+    const fileNameOnServer = file?.match(/(.*)\/([^/]*)$/)[2] // text after last slash
+    const fileExtension = file?.match(/[^.]+$/)[0] // text after last .
+    const imgExtensions = ["jpeg", "jpg", "png", "gif", "bmp", "tiff", "tif", "psd", "eps", "svg", "webp", "heif", "heic", "jfif", "jfi", "jpe", "jpf", "jpx", "jp2", "j2k", "jpc", "pgf", "raf", "raw", "cr2", "nef", "orf", "arw", "srf", "sr2", "dng"]
+
     return (
-        <img key={file} src={file} className="w100p p15" />
+        imgExtensions?.includes(fileExtension)
+            ?
+            <img key={file} src={file} className="w100p p15" />
+            :
+            <a href={file} download className="w100p p15 fw600">{fileNameOnServer}</a>
     )
 }
--- src/components/messages/useUploadedPastedDroppedFilesToMsgFiles.js
+++ src/components/messages/useUploadedPastedDroppedFilesToMsgFiles.js
@@ -7,7 +7,8 @@ export default function useUploadedPastedDroppedFilesToMsgFiles() {
 
   function uploadedPastedDroppedFilesToMsgFiles(files) {
     Object.values(files)?.map((file, fileInd) => {
-      const fileRenamed = new File([file], file.name.split(".")[0] + Date.now() + "." + file.name.split(".")[1]) // eg: image1695902498918.png
+      // remove all dots not last . + add unix to name
+      const fileRenamed = new File([file], file.name.replace(/\.(?!$)/g, "").split(".")[0] + Date.now() + "." + file.name.split(".")[1]) // eg: image1695902498918.png
       if (fileInd === 0) {
         messagesSet(prev => prev.map((message, ind) => ind === 0 && ({ ...message, file: fileRenamed }))) // in 1 message leave prev msg (text) + add 1 file
       } else {
