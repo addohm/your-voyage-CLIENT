d37965b32de97a46df3aa076afc132034b8b8020
enotowitch <en.enotowitch@gmail.com>
2024-01-19 18:55:23 +0200

    F: useLang 2

--- src/components/user/useLang.js
+++ src/components/user/useLang.js
@@ -5,11 +5,16 @@ export default function useLang() {
     const [lang, langSet] = useState([])
 
     useEffect(() => {
-        // set default lang from browser
+        const availableLangs = ["en", "ch"]
+        // NO LANG SELECTED AT ALL => set default lang from browser
         if (!localStorage.getItem("lang")) {
             let preferredLang = (navigator.language || navigator.userLanguage).replace(/-.*/, '')
-            const availableLangs = ["en", "ch"]
             preferredLang = availableLangs.includes(preferredLang) ? preferredLang : "en"
+            localStorage.setItem("lang", preferredLang);
+        } else {
+            // WRONG LANG SELECTED: this lang is not in availableLangs
+            let preferredLang = localStorage.getItem("lang")
+            if (!availableLangs.includes(preferredLang)) preferredLang = "en"
             localStorage.setItem("lang", preferredLang);
         }
 
