f8d34401367beb85e09f6df64575806e8131b2ed
enotowitch <en.enotowitch@gmail.com>
2024-01-23 16:05:27 +0200

    M: Burger: no messages => show Burger, with messages => show UserImgWithNotReadNum

--- src/components/burger/Burger.js
+++ src/components/burger/Burger.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import "./index.scss";
+import UserImgWithNotReadNum from '../other/UserImgWithNotReadNum';
+import { Context } from '../../Context';
+import { Link } from 'react-router-dom';
+import { MESSAGES_ROUTE } from '../../utils/consts';
 
 export default function Burger({ isVisible, isVisibleMobileNavSet, isVisibleMobileNav }) {
+
+    const { user, totalNotReadNum } = useContext(Context)
+
+    // no messages => show Burger, with messages => show UserImgWithNotReadNum
     return (
-        isVisible &&
-        <div className="hamburger por zi3" >
-            <input
-                className="label-check"
-                id="label-check"
-                type="checkbox"
-                onClick={() => isVisibleMobileNavSet(prev => !prev)}
-                checked={isVisibleMobileNav}
-            />
-            <label htmlFor="label-check" className="hamburger-label">
-                <div className="line1 bg_black"></div>
-                <div className="line2 bg_black"></div>
-                <div className="line3 bg_black"></div>
-            </label>
-        </div>
+        (totalNotReadNum === 0 || totalNotReadNum.length === 0)
+            ?
+            isVisible &&
+            <>
+                <div className="hamburger por zi3" >
+                    <input
+                        className="label-check"
+                        id="label-check"
+                        type="checkbox"
+                        onClick={() => isVisibleMobileNavSet(prev => !prev)}
+                        checked={isVisibleMobileNav}
+                    />
+                    <label htmlFor="label-check" className="hamburger-label">
+                        <div className="line1 bg_black"></div>
+                        <div className="line2 bg_black"></div>
+                        <div className="line3 bg_black"></div>
+                    </label>
+                </div>
+            </>
+            :
+            <Link to={MESSAGES_ROUTE} onClick={() => isVisibleMobileNavSet(false)}>
+                <UserImgWithNotReadNum img={user?.img} notReadNum={totalNotReadNum} imgClassName="w40 h40" numClassName="w20 h20 l-4 t-2 fz13" />
+            </Link>
     );
 };
--- src/components/other/UserImgWithNotReadNum.js
+++ src/components/other/UserImgWithNotReadNum.js
@@ -1,8 +1,8 @@
 export default function UserImgWithNotReadNum({ img, notReadNum, imgClassName, numClassName }) {
     return (
-        <>
+        <div className="por">
             {notReadNum > 0 && <div className={`poa l4 t0 fcc bg_brand white br50 ${numClassName}`}>{notReadNum}</div>}
             <img className={`br50 ${imgClassName}`} src={img} />
-        </>
+        </div>
     )
 }
