c33621c5a65db9900f47111634bd047732b8435b
enotowitch <en.enotowitch@gmail.com>
2024-01-25 09:35:09 +0200

    M: AddCoursesPage: course mobile style

--- src/components/pages/addCourses/AddCoursesPage.js
+++ src/components/pages/addCourses/AddCoursesPage.js
@@ -19,20 +19,22 @@ export default function AddCoursesPage() {
             <div className="title mb w100p tac">Add Courses</div>
             {Array.from(posts)?.map(({ courseName, coachEmail, coachName, price, discountPrice, order, courseLabel, _id }, ind) => {
                 return (
-                    <div key={_id} className="fcc g15">
+                    <div key={_id} className="fc g15 bsh brL pl15 pr15 pt15 pb30">
                         {/* ! _id must be before last input */}
                         <input hidden value={_id} name="_id" />
-                        <Button type="submit"><Save className={`${_id ? "brand" : ""}`} /></Button>
+                        <div className="f jcsb">
+                            <Button style={{ margin: 0, padding: 0, minWidth: 0, marginBottom: 5 }} type="submit"><Save className={`${_id ? "brand" : ""}`} /></Button>
+                            <Close onClick={() => deleteCourse(ind, _id)} />
+                        </div>
                         <Input className="fz16" defaultValue={courseName} name="courseName" placeholder="course name" />
                         <Input className="fz16" defaultValue={coachEmail} name="coachEmail" placeholder="coach email" />
                         <Input className="fz16" defaultValue={coachName} name="coachName" placeholder="coach name" />
-                        <Input className="fz16 w80" defaultValue={price} name="price" placeholder="price" />
-                        <Input className="fz16 w80" defaultValue={discountPrice} name="discountPrice" placeholder="discount" />
-                        <Input className="fz16 w80" defaultValue={order} name="order" placeholder="order" />
+                        <Input className="fz16" defaultValue={price} name="price" placeholder="price" />
+                        <Input className="fz16" defaultValue={discountPrice} name="discountPrice" placeholder="discount" />
+                        <Input className="fz16" defaultValue={order || ind + 1} name="order" placeholder="order" />
                         {/* ! add new fields BEFORE last input  */}
-                        <Input className="fz16 w80" defaultValue={courseLabel} name="courseLabel" placeholder="label" />
+                        <Input className="fz16" defaultValue={courseLabel} name="courseLabel" placeholder="label" />
                         {/* TODO !!! delete course */}
-                        <Close onClick={() => deleteCourse(ind, _id)} />
                     </div>
                 )
             })}
