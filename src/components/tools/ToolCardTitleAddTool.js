9e339ac2e03f4c61b2256dd4fffaaf82edcfe82a
enotowitch <en.enotowitch@gmail.com>
2024-01-27 14:03:31 +0200

    !F: useMessageSnackbar

--- src/components/tools/ToolCardTitleAddTool.js
+++ src/components/tools/ToolCardTitleAddTool.js
@@ -1,19 +1,22 @@
 import { Add as AddTool } from '@mui/icons-material';
 import { Link } from "react-router-dom";
 import Animation from '../animation/Animation';
+import For from '../other/For';
 
 export default function ToolCardTitleAddTool({ thisTitleClicked, iconSize, name }) {
     return (
         thisTitleClicked &&
-        <Animation
-            type="scale"
-        >
-            <Link to={`/add/${name}`}>
-                <AddTool
-                    style={{ height: iconSize, width: iconSize }}
-                    className='poa t0'
-                />
-            </Link>
-        </Animation>
+        <For role="admin">
+            <Animation
+                type="scale"
+            >
+                <Link to={`/add/${name}`}>
+                    <AddTool
+                        style={{ height: iconSize, width: iconSize }}
+                        className='poa t0'
+                    />
+                </Link>
+            </Animation>
+        </For>
     )
 }
