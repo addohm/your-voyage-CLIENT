caa729931f4ab43b024be0a0efbb8d2af0f0f697
enotowitch <en.enotowitch@gmail.com>
2024-01-28 10:57:23 +0200

    M

--- src/components/appRouter/useRoutes.js
+++ src/components/appRouter/useRoutes.js
@@ -29,7 +29,7 @@ export default function useRoutes() {
 	}, [user])
 
 	useEffect(() => {
-		window.scrollTo(0, 0)
+		setTimeout(() => window.scrollTo(0, 0), 1)
 	}, [location])
 
 	return (
--- src/components/messages/useMessageSnackbar.js
+++ src/components/messages/useMessageSnackbar.js
@@ -12,7 +12,7 @@ export default function useMessageSnackbar(snackbarSet, user) {
 
             if (!user?._id || !userId) return // onLoad user is null for few ms
             const isReceivedMsgMyMsg = user?._id === userId
-            if (isReceivedMsgMyMsg) return // don't show my messages in snackbar
+            // TODO !!! if (isReceivedMsgMyMsg) return // don't show my messages in snackbar
             if (!isVisibleSnackbar) return // if user clicked "close snackbar": don't show snackbar until page reload
 
             snackbarSet({
