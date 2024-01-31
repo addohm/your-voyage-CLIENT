c905ac31c7b37ce694e17547768b8d327e5b04a9
enotowitch <en.enotowitch@gmail.com>
2024-01-31 16:50:33 +0200

    M

--- src/components/footer/FooterLink.js
+++ src/components/footer/FooterLink.js
@@ -6,7 +6,6 @@ export default function FooterLink({ text, type }) { // type: "terms" or "privac
     const id = useGetLastTermsOrPrivacyId(type)
 
     return (
-        id && // if terms or privacy exist
         <Link2 to={`/post/${type}/${id}`}>
             {text}
         </Link2>
