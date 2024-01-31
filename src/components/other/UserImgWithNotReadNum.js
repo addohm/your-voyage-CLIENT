f26be6c10d65087f1481e92fd6246ede24fc2ac4
enotowitch <en.enotowitch@gmail.com>
2024-01-31 16:24:08 +0200

    M: UserImgWithNotReadNum: +useGenUserImgDepOnFirstChar if no userImg(Login via email, not Google)

--- src/components/other/UserImgWithNotReadNum.js
+++ src/components/other/UserImgWithNotReadNum.js
@@ -1,8 +1,18 @@
+import useGenUserImgDepOnFirstChar from "../../hooks/useGenUserImgDepOnFirstChar"
+
 export default function UserImgWithNotReadNum({ img, notReadNum, imgClassName, numClassName }) {
+
+    const userImg = useGenUserImgDepOnFirstChar()
+
     return (
         <div className="por">
             {notReadNum > 0 && <div className={`poa l4 t0 fcc bg_brand white br50 ${numClassName}`}>{notReadNum}</div>}
-            <img className={`br50 ${imgClassName}`} src={img} />
+            {img
+                ?
+                <img className={`br50 ${imgClassName}`} src={img} />
+                :
+                userImg
+            }
         </div>
     )
 }
