b1e99cf4f5ed7ff92aadc6a7c70fcc92ddfc62d0
enotowitch <en.enotowitch@gmail.com>
2024-02-06 15:40:27 +0200

    M: CoursesMainPage

--- src/components/coachCard/CoursesMainPage.js
+++ src/components/coachCard/CoursesMainPage.js
@@ -3,7 +3,7 @@ import t from '../../hooks/useT'
 
 export default function CoursesMainPage() {
     return (
-        <div className='h100vh fcc mb200'>
+        <div className='mih100vh fcc mb200'>
             <div>
                 <div className='title tac mb15 ls1'>{t("Choose your course")}:</div>
                 <Courses />
