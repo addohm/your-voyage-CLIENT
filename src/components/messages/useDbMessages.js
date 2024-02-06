15584d1a217fcfe362876c5b60152c77bbbd0469
enotowitch <en.enotowitch@gmail.com>
2024-02-06 15:14:40 +0200

    F: useDbMessages: goToBottom when dbMessages is loaded ONE time

--- src/components/messages/useDbMessages.js
+++ src/components/messages/useDbMessages.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import axios from "../../utils/axios"
 import useInterval from "../../hooks/useInterval"
 import { useLocation } from "react-router-dom"
+import goToBottom from "../../utils/goToBottom"
 
 export default function useDbMessages() {
 
@@ -9,16 +10,27 @@ export default function useDbMessages() {
     const { interval } = useInterval()
 
     const [dbMessages, dbMessagesSet] = useState([])
+    const [isDbMessagesLoaded, isDbMessagesLoadedSet] = useState(false)
 
     useEffect(() => {
         if (!token) return
         async function getMessages() {
             const res = await axios("/getMessages", { token })
-            res && dbMessagesSet(res)
+            if (!res) return
+            dbMessagesSet(res)
+            isDbMessagesLoadedSet(true)
         }
 
         getMessages()
     }, [interval])
+
+    // goToBottom when dbMessages is loaded ONE time
+    useEffect(() => {
+        if (!isDbMessagesLoaded) return
+        setTimeout(() => {
+            goToBottom()
+        }, 1000);
+    }, [isDbMessagesLoaded])
 
     // clean messages when changing the room
     useEffect(() => dbMessagesSet([]), [token])
