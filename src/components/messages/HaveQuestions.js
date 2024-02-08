da5f8a43fca74f27e626112f7b7eec08eea89b85
enotowitch <en.enotowitch@gmail.com>
2024-02-08 16:00:32 +0200

    F: HaveQuestions: For role="user"

--- src/components/messages/HaveQuestions.js
+++ src/components/messages/HaveQuestions.js
@@ -1,13 +1,16 @@
 import { FAQ_ROUTE, SUPPORTS_ROUTE } from "../../utils/consts";
 import Link2 from "../other/Link2";
 import t from "../../hooks/useT";
+import For from "../other/For";
 
 export default function HaveQuestions() {
     return (
-        <div className="fc aic wfc m0a pb20">
-            <div className="title2">{t("Have any questions")}?</div>
-            <Link2 to={SUPPORTS_ROUTE}>- {t("Visit our Support")}</Link2>
-            <Link2 to={FAQ_ROUTE}>- {t("Visit our FAQ")}</Link2>
-        </div>
+        <For role="user">
+            <div className="fc aic wfc m0a pb20">
+                <div className="title2">{t("Have any questions")}?</div>
+                <Link2 to={SUPPORTS_ROUTE}>- {t("Visit our Support")}</Link2>
+                <Link2 to={FAQ_ROUTE}>- {t("Visit our FAQ")}</Link2>
+            </div>
+        </For>
     )
 }
