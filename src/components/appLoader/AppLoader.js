53cf36a1591cd0dab25481239421132214689aab
enotowitch <en.enotowitch@gmail.com>
2024-02-08 14:02:38 +0200

    F: AppLoader: test commit 2

--- src/components/appLoader/AppLoader.js
+++ src/components/appLoader/AppLoader.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import Dots from "../dots/Dots"
 import MainLogo from "../header/MainLogo";
+import useWindowSize from "../../hooks/useWindowSize";
 
 export default function AppLoader() {
 
-    // test commit
     const [isAppLoaded, isAppLoadedSet] = useState(false)
     const [isTimePassed, isTimePassedSet] = useState(false)
+    const { isMobile } = useWindowSize()
 
     const handleSiteLoad = () => isAppLoadedSet(true)
     useEffect(() => {
@@ -17,6 +18,7 @@ export default function AppLoader() {
 
     // fadeOut = isAppLoaded && isTimePassed 
     return (
+        !isMobile &&
         <div className={`fcc pof t0 l0 zi10 w100p h100p miw100vw mih100vh bg_white appLoaderAnimation ${isAppLoaded && isTimePassed ? "appLoaderAnimationFadeOut" : ""}`}>
             <MainLogo className="fz30" />
             <Dots className="mt30 fz50 opAnim10" />
