bfd437bf8abbfab7f031f5a7cb1083331bc1306d
enotowitch <en.enotowitch@gmail.com>
2024-02-09 14:27:17 +0200

    M: FAQs: +isLoading

--- src/components/faq/FAQs.js
+++ src/components/faq/FAQs.js
@@ -4,24 +4,29 @@ import usePosts from "../post/usePosts"
 import FAQ from "./FAQ"
 import t from "../../hooks/useT"
 import NoPostsYet from "../other/NoPostsYet"
+import Loading from "../other/Loading"
 // ! DO NOT DELETE imports
 
 export default function FAQs() {
 
-    const { posts } = usePosts({ type: "faq" })
+    const { posts, isLoading } = usePosts({ type: "faq" })
 
     return (
         // UNCOMMENT for 1: "non-closing" FAQs
         // <Posts Post={FAQ} type="faq" className="fc aic" />
         // 2: "closing" FAQs: after each FAQ clicked, previous FAQ will be closed
-        posts?.length > 0 ?
-            <>
-                <div className="title tac mb30">{t("FAQ")}:</div>
-                <div className="fc aic">
-                    <Accordion posts={posts} className="bg_white zi2 faq cardAnim" />
-                </div>
-            </>
-            :
-            <NoPostsYet isVisible={true} />
+        <Loading isLoading={isLoading}>
+            {posts?.length > 0
+                ?
+                <>
+                    <div className="title tac mb30">{t("FAQ")}:</div>
+                    <div className="fc aic">
+                        <Accordion posts={posts} className="bg_white zi2 faq cardAnim" />
+                    </div>
+                </>
+                :
+                <NoPostsYet isVisible={true} />
+            }
+        </Loading>
     )
 }
