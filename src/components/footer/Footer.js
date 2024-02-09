1131f42051a09d8ab8b5c4968eb44ab7fde69dbc
enotowitch <en.enotowitch@gmail.com>
2024-02-09 10:03:38 +0200

    F: Footer: hide when isVisibleMobileNav

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 import { MAIN_ROUTE } from '../../utils/consts'
 import "./index.scss"
 import FooterSocialLinks from './FooterSocialLinks'
 import FooterLinks from './FooterLinks'
 import FooterLogo from './FooterLogo'
+import { Context } from '../../Context'
 
 export default function Footer() {
 
@@ -12,10 +13,11 @@ export default function Footer() {
     const mainRouteStyle = "poa b0 l0"
     const location = useLocation().pathname
     // not visible in /support/ & /message/ routes.
-    const isVisible = !location.includes("/support/") && !location.includes("/message/")
+    const isVisibleForLocations = !location.includes("/support/") && !location.includes("/message/")
+    const { isVisibleMobileNav } = useContext(Context)
 
     return (
-        isVisible &&
+        isVisibleForLocations && !isVisibleMobileNav &&
         <footer className={`por fcc fwn jcsb mt100 pt pb px100 w100p ${location === MAIN_ROUTE ? mainRouteStyle : ""} footer`}>
             {/* LEFT */}
             <FooterLogo />
