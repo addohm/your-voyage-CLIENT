e0e1df97b0b44b521860b1215a0171b28b43f5d8
enotowitch <en.enotowitch@gmail.com>
2024-02-11 14:41:34 +0200

    F: CourseBuyBtn: toUpperCase

--- src/components/coachCard/CourseBuyBtn.js
+++ src/components/coachCard/CourseBuyBtn.js
@@ -7,7 +7,7 @@ export default function CourseBuyBtn({ isCourseHovered, courseLabelColor }) {
             className={`poa b-15 fcc h50 ls2 w300 bblr5 bbrr5 op0 white fw600 hoverHeight55 ${isCourseHovered ? "courseBuyButtonAnimationFadeIn" : "courseBuyButtonAnimationFadeOut"}`}
             style={{ background: courseLabelColor }}
         >
-            {t("Buy")}
+            {t("Buy").toUpperCase()}
         </div>
     )
 }
