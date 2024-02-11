8f0547341d1f591db54b06e2738a33c8e02805fa
enotowitch <en.enotowitch@gmail.com>
2024-02-11 14:47:38 +0200

    M: DialogIcons

--- src/components/dialog/DialogIcons.js
+++ src/components/dialog/DialogIcons.js
@@ -8,7 +8,7 @@ export default function DialogIcons({ className = "f jcsb", backIconClassName = 
     const { onClose } = dialog
 
     return (
-        <div className={className || ""}>
+        <div className={`opAnim10 ${className || ""}`}>
             {dialog?.onBack && <ArrowBack className={backIconClassName || ""} onClick={() => dialog?.onBack?.()} />}
             <Close className={closeIconClassName || ""} onClick={() => onClose || dialogSet({ show: false })} />
         </div>
